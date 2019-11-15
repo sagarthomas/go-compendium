19c3058531634125fba5bbef94899d9ed3c730a3
Sagar Thomas <sagarthomas27@gmail.com>
2019-11-14 22:29:24 -0500

    project euler q5 brute force

--- project_euler/problem_5/smallest_multiple.go
+++ project_euler/problem_5/smallest_multiple.go
@@ -16,22 +16,48 @@ import "fmt"
 func main() {
 
 	// For each number from 1 -> 20 we can create a list of all factors of that number for faster lookup time later on
-	factorTable := make(map[int][]int)
-	for i := 1; i <= 20; i++ {
-		factorTable[i] = findFactors(i)
-	}
-
+	// factorTable := make(map[int][]int)
+	// for i := 1; i <= 20; i++ {
+	// 	factorTable[i] = findFactors(i)
+	// }
 	current := 20
-	answer := 0
+	failedDivisor := make(chan int)
 	for {
-
+		go checkMultiples(current, failedDivisor)
+		x := <-failedDivisor
+		if x == 0 {
+			break
+		}
 		current += 20 // increment by 20 since we don't care about values that aren't divisible by 20
+
+		fmt.Println("======")
 	}
 
-	fmt.Println(answer)
+	fmt.Println(current)
 }
 
 func findFactors(i int) []int {
 	// There should be less factors than the sqrt of i but we'll implement that later
+	result := make([]int, 0)
+	for j := i; j > 0; j-- {
+		if i%j == 0 {
+			result = append(result, j)
+		}
+	}
+	fmt.Printf("factors of %v: %v\n", i, result)
+	return result
+}
 
+func checkMultiples(v int, c chan int) bool {
+	// create a simple int-bool map for traking values 1-20
+
+	for i := 2; i < 20; i++ {
+		if v%i != 0 {
+			fmt.Printf("%v is not divisble by %v\n", v, i)
+			c <- i
+			return false
+		}
+	}
+	c <- 0
+	return true
 }
