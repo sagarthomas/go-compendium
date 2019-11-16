2eca7ec216c876ef69f48350d099a5b40dc110b5
Sagar Thomas <sagarthomas27@gmail.com>
2019-11-15 23:18:47 -0500

    Optimized euler q5 using lcm formula

--- project_euler/problem_5/smallest_multiple.go
+++ project_euler/problem_5/smallest_multiple.go
@@ -7,57 +7,45 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// Things we know:
-// 1. All numbers are divisible by 1 so we don't need to waste time computing that
-// 2. even numbers are divisible by 2 etc
-// 3. if some value b is divisble by a, b is also divisble by the factors of a. ex. 40 % 20 == 0 -> 40 % (10, 5, 4, 2, 1) == 0
+// Originally implemented brute force by iterating by 20 and checking if the value is evenly divisible by 2..19
+// This can be further optimized by checking numbers 11..19 only. This is because -> if a % b == 0, then a % factors(b) == 0
+// Since the range 11..19 contains numbers that are composite values from 1..10, a value being divisible by 11..19 is divisible
+// by 1..10.
+// To optimize even further, We notice that we are essentially looking for the LCM of numbers 1..20 and we can do so by
+// using the forumula LCM(a, b) = a * b/ gcd(a, b) which can be extended to LCM(a, b, c) = LCM(a, LCM(b,c)), where gcd is greatest
+// common divisor
 func main() {
-
-	// For each number from 1 -> 20 we can create a list of all factors of that number for faster lookup time later on
-	// factorTable := make(map[int][]int)
-	// for i := 1; i <= 20; i++ {
-	// 	factorTable[i] = findFactors(i)
-	// }
-	current := 20
-	failedDivisor := make(chan int)
-	for {
-		go checkMultiples(current, failedDivisor)
-		x := <-failedDivisor
-		if x == 0 {
-			break
-		}
-		current += 20 // increment by 20 since we don't care about values that aren't divisible by 20
-
-		fmt.Println("======")
+	start := time.Now()
+	a := make([]int, 20)
+	for i := 0; i < 20; i++ {
+		a[i] = i + 1
 	}
 
-	fmt.Println(current)
+	// fmt.Println(a)
+	fmt.Printf("Smallest positive number is %v in %vs", multiLCM(a), time.Since(start).Seconds())
 }
 
-func findFactors(i int) []int {
-	// There should be less factors than the sqrt of i but we'll implement that later
-	result := make([]int, 0)
-	for j := i; j > 0; j-- {
-		if i%j == 0 {
-			result = append(result, j)
-		}
+func multiLCM(values []int) int {
+	if len(values) == 1 {
+		return values[0]
 	}
-	fmt.Printf("factors of %v: %v\n", i, result)
-	return result
+	return lcm(values[0], multiLCM(values[1:]))
 }
 
-func checkMultiples(v int, c chan int) bool {
-	// create a simple int-bool map for traking values 1-20
+func lcm(a int, b int) int {
+	return (a * b) / gcd(a, b)
+}
 
-	for i := 2; i < 20; i++ {
-		if v%i != 0 {
-			fmt.Printf("%v is not divisble by %v\n", v, i)
-			c <- i
-			return false
-		}
+func gcd(a int, b int) int {
+	for b != 0 {
+		temp := a
+		a = b
+		b = temp % b
 	}
-	c <- 0
-	return true
+	return a
 }
